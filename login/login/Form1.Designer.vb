79f1024ec8e6eb74a8530bb377661044c49ea6c0
VIKAS S <113232410+VIKAS0274@users.noreply.github.com>
2023-07-11 22:35:36 +0530

    Add files via upload

---
+++ login/login/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.login = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.UsernameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PasswordDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VkBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LogindataDataSet = New login.logindataDataSet()
+        Me.VkTableAdapter = New login.logindataDataSetTableAdapters.vkTableAdapter()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VkBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LogindataDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(291, 117)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "vikas"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(291, 181)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(193, 117)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(213, 202)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "password"
+        '
+        'login
+        '
+        Me.login.Location = New System.Drawing.Point(292, 270)
+        Me.login.Name = "login"
+        Me.login.Size = New System.Drawing.Size(75, 23)
+        Me.login.TabIndex = 4
+        Me.login.Text = "login"
+        Me.login.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.UsernameDataGridViewTextBoxColumn, Me.PasswordDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.VkBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(516, 253)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 5
+        '
+        'UsernameDataGridViewTextBoxColumn
+        '
+        Me.UsernameDataGridViewTextBoxColumn.DataPropertyName = "username"
+        Me.UsernameDataGridViewTextBoxColumn.HeaderText = "username"
+        Me.UsernameDataGridViewTextBoxColumn.MinimumWidth = 6
+        Me.UsernameDataGridViewTextBoxColumn.Name = "UsernameDataGridViewTextBoxColumn"
+        Me.UsernameDataGridViewTextBoxColumn.Width = 125
+        '
+        'PasswordDataGridViewTextBoxColumn
+        '
+        Me.PasswordDataGridViewTextBoxColumn.DataPropertyName = "password"
+        Me.PasswordDataGridViewTextBoxColumn.HeaderText = "password"
+        Me.PasswordDataGridViewTextBoxColumn.MinimumWidth = 6
+        Me.PasswordDataGridViewTextBoxColumn.Name = "PasswordDataGridViewTextBoxColumn"
+        Me.PasswordDataGridViewTextBoxColumn.Width = 125
+        '
+        'VkBindingSource
+        '
+        Me.VkBindingSource.DataMember = "vk"
+        Me.VkBindingSource.DataSource = Me.LogindataDataSet
+        '
+        'LogindataDataSet
+        '
+        Me.LogindataDataSet.DataSetName = "logindataDataSet"
+        Me.LogindataDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'VkTableAdapter
+        '
+        Me.VkTableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.login)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VkBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LogindataDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents login As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents LogindataDataSet As logindataDataSet
+    Friend WithEvents VkBindingSource As BindingSource
+    Friend WithEvents VkTableAdapter As logindataDataSetTableAdapters.vkTableAdapter
+    Friend WithEvents UsernameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents PasswordDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+End Class
---
+++ login/login/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data.SqlClient
+Imports System.Windows
+
+Public Class Form1
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim dt As DataTable
+    Dim dr As SqlDataReader
+    Dim da As New SqlDataAdapter
+    Dim str As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LogindataDataSet.vk' table. You can move, or remove it, as needed.
+        Me.VkTableAdapter.Fill(Me.LogindataDataSet.vk)
+        con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\vb\login\login\logindata.mdf;Integrated Security=True"
+
+
+    End Sub
+
+    Private Sub login_Click(sender As Object, e As EventArgs) Handles login.Click
+        con.Open()
+        str = "Select * from vk where username='" & TextBox1.Text & "' and password ='" & TextBox2.Text & "'"
+        cmd = New SqlCommand(str, con)
+        da = New SqlDataAdapter(cmd)
+        dt = New DataTable
+        da.Fill(dt)
+        If (dt.Rows.Count > 0) Then
+            MsgBox("Entry accessed")
+            Form2.Show()
+        Else
+            MsgBox("Invalid Username & Password")
+        End If
+        cmd.ExecuteNonQuery()
+        cmd.Dispose()
+        con.Close()
+
+    End Sub
+End Class
